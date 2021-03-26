bd04dfb0ce0f41d27a9137a6d63f534108f1917e
Rodrigo Zapiello <rzapiello@nexocs.com>
2021-03-26 17:59:00 -0300

    Update Restaurante.java

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -11,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
  
 
@@ -37,10 +38,10 @@ public class Restaurante {
 	@JoinColumn(name ="cozinha_id",nullable = false)
 	private Cozinha cozinha;
 	
-	//@ManyToOne
-	//@JoinTable(name = "restaurante_forma_pagamento",
-	//		joinColumns = @JoinColumn(name ="restaurante_id"),
-	//		inverseJoinColumns =  @JoinColumn(name ="forma_pagamento_id"))
-	//private List<FormaPagamento>  formasPagamento = new ArrayList<>();
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name ="restaurante_id"),
+			inverseJoinColumns =  @JoinColumn(name ="forma_pagamento_id"))
+	private List<FormaPagamento>  formasPagamento = new ArrayList<>();
 	
 }
