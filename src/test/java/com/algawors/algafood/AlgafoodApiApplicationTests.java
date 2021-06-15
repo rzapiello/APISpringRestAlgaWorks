1af86a38dc10a06bd53d11f469988367a2577415
Rodrigo Zapiello <rzapiello@nexocs.com>
2021-06-15 18:37:45 -0300

    Update AlgafoodApiApplicationTests.java

--- src/test/java/com/algawors/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algawors/algafood/AlgafoodApiApplicationTests.java
@@ -1,12 +1,17 @@
 package com.algawors.algafood;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AlgafoodApiApplicationTests {
 
-	//@Test
-	//void contextLoads() {
-	//}
+	@Test
+	public void contextLoads() {
+	}
 
 }
